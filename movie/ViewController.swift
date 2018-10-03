cb2bfb2190ee73e53a51a4ac0d2cf9929982cccb
ninonanoni2038 <ninonanoni2038@gmail.com>
2018-10-03 15:38:36 +0900

    Initial Commit

---
+++ movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  movie
+//
+//  Created by 二宮啓 on 2018/10/03.
+//  Copyright © 2018年 SatoshiNinomiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
